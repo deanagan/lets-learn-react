0485dffed4ee72542203756a1e25b7ce58cd2c09
Dean Agan <agandfr@gmail.com>
2022-02-02 18:55:50 +1100

    Code cleanup. No functional change.

--- src/Contents/TableOfContents.jsx
+++ src/Contents/TableOfContents.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const StyledOrderedList = styled.ol`
   li {
@@ -10,33 +10,34 @@ const StyledOrderedList = styled.ol`
 
 // `;
 
-
-
-const FragmentedList = ({items}) => (
+const FragmentedList = ({ items }) => (
   <StyledOrderedList>
-    {items.map((content) => (<li className="fragment" key={content.id}>{content.value}</li>))}
+    {items.map((content) => (
+      <li className="fragment" key={content.id}>
+        {content.value}
+      </li>
+    ))}
   </StyledOrderedList>
 );
 
-
-
 const contents = [
-  { id: 1, value: "React's phases to updating the DOM"},
-  { id: 2, value: "Code structure - our plan A to improve performance"},
-  { id: 3, value: "React.memo - when we want to avoid component update"},
-  { id: 4, value: "useMemo and useCallback - optimisation tools"},
-  { id: 5, value: "useEffect and useLayoutEffect - asynchronous and synchronous"},
-  { id: 6, value: "useState and useReducer - keeping track of states"},
-  { id: 7, value: "References"},
-]
+  { id: 1, value: "React's phases to updating the DOM" },
+  { id: 2, value: "Code structure - our plan A to improve performance" },
+  { id: 3, value: "React.memo - when we want to avoid component update" },
+  { id: 4, value: "useMemo and useCallback - optimisation tools" },
+  {
+    id: 5,
+    value: "useEffect and useLayoutEffect - asynchronous and synchronous",
+  },
+  { id: 6, value: "useState and useReducer - keeping track of states" },
+  { id: 7, value: "References" },
+];
 
 export default function TableOfContents() {
-
-
   return (
     <section>
-    <h3>Contents:</h3>
+      <h3>Contents:</h3>
       <FragmentedList items={contents} />
     </section>
-  )
+  );
 }
