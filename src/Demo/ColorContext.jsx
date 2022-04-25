6142fe5c980421ffe71abb1a9758076309f41c68
Dean Agan <agandfr@gmail.com>
2022-04-25 20:00:02 +1000

    Use context work

--- src/Demo/ColorContext.jsx
+++ src/Demo/ColorContext.jsx
@@ -1,4 +1,28 @@
 import React, { createContext, useContext, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+// Additive devices that are light emitting use RGB. Such as Computers, Television, Mobile Phone
+const rgbColors = [
+  { name: "red", uniqueId: 11 },
+  { name: "green", uniqueId: 12 },
+  { name: "blue", uniqueId: 13 },
+];
+
+// Subtractive CMYK is used for paper, items that reflect light
+const cmykColors = [
+  { name: "cyan", uniqueId: 21 },
+  { name: "magenta", uniqueId: 22 },
+  { name: "yellow", uniqueId: 23 },
+  { name: "black", uniqueId: 24 },
+];
+
+export const SUBTRACTIVE_COLOR_TYPE = "Subtractive";
+export const ADDITIVE_COLOR_TYPE = "Additive";
+
+const colorMapping = [
+  { name: SUBTRACTIVE_COLOR_TYPE, values: cmykColors, uniqueId: uuidv4() },
+  { name: ADDITIVE_COLOR_TYPE, values: rgbColors, uniqueId: uuidv4() },
+];
 
 const SET_SELECTED_COLOR_TYPE = "SET_SELECTED_COLOR_TYPE";
 const SET_SELECTED_COLOR = "SET_SELECTED_COLOR";
@@ -11,6 +35,9 @@ function colorContextReducer(state, action) {
       return {
         ...state,
         colorType: action.colorType,
+        colors: colorMapping
+          .find((cm) => cm.name === action.colorType)
+          .values.map((cv) => cv.name),
       };
     }
 
@@ -27,13 +54,27 @@ function colorContextReducer(state, action) {
   }
 }
 
-const initialState = {
+let defaultInitialState = {
   colorType: "",
   selectedColor: "",
+  colors: [],
+  colorTypeChoices: colorMapping.map((cm) => cm.name),
 };
 
-function ColorProvider({ children }) {
-  const [state, dispatch] = useReducer(colorContextReducer, initialState);
+function ColorProvider({ children, initialColorType }) {
+  const colors = colorMapping
+    .find((cm) => cm.name === initialColorType)
+    .values.map((cv) => cv.name);
+
+  defaultInitialState.colorType = initialColorType;
+  defaultInitialState.colors = colors;
+  defaultInitialState.selectedColor = colors[0];
+
+  const [state, dispatch] = useReducer(
+    colorContextReducer,
+    defaultInitialState
+  );
+
   const value = { state, dispatch };
   return (
     <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
