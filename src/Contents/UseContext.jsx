698889323854f0db4a2b31001a8df5bcdefb336d
Dean Agan <agandfr@gmail.com>
2022-05-08 19:21:26 +1000

    Adding in code

--- src/Contents/UseContext.jsx
+++ src/Contents/UseContext.jsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 
 import CodeDemo from "../Common/CodeDemo";
-import UsingUseContextDemo from "../Demo/UsingUseContextDemo";
+//import UsingUseContextDemo from "../Demo/UsingUseContextDemo";
+import UsingUseContextPropDrillDemo from "../Demo/UsingUseContextPropDrillDemo";
 import { dedentStrUsing1stLineIndent } from "../Utils/util";
 
 const Header = styled.h3`
@@ -37,7 +38,102 @@ const useContextCodes = [
         uniqueId: uuidv4(),
         lineNumbers: "",
         src: dedentStrUsing1stLineIndent(`
-        // Show me some code
+          export default function UsingUseContextPropDrillDemo() {
+            const [colorTypeSetting, { setColor, setColorType }] =
+              useColorType(ADDITIVE_COLOR_TYPE);
+
+            const onHandleColorTypeSelection = useCallback(
+              (colorType) => setColorType(colorType),
+              [setColorType]
+            );
+
+            const onHandleColorSelection = useCallback(
+              (color) => setColor(color),
+              [setColor]
+            );
+
+            const colorTypeChoices = useMemo(
+              () =>
+                colorMapping.filter((cm) => cm.name !== colorTypeSetting.colorType.name),
+              [colorTypeSetting.colorType.name]
+            );
+
+            return (
+              <div style={{ display: "block" }}>
+                <ColoredHeader color={colorTypeSetting.selectedColor.name}>
+                  This header changes color (Prop Drilling)
+                </ColoredHeader>
+                <DropDown
+                  id="color-type"
+                  dropDownLabelId="color-type"
+                  choices={colorTypeChoices}
+                  currentValue={colorTypeSetting.colorType}
+                  setValues={onHandleColorTypeSelection}
+                />
+                <DropDown
+                  id="colors"
+                  dropDownLabelId="colors"
+                  choices={colorTypeSetting.colors}
+                  currentValue={colorTypeSetting.selectedColor}
+                  setValues={onHandleColorSelection}
+                />
+                <DescriptionAccordions
+                  colors={colorTypeSetting.colors}
+                  colorType={colorTypeSetting.colorType}
+                />
+              </div>
+            );
+          }
+        `),
+      },
+      {
+        uniqueId: uuidv4(),
+        lineNumbers: "",
+        src: dedentStrUsing1stLineIndent(`
+          function ColorAccordion({ colors }) {
+            const memoizedColors = useMemoObjCompare(colors);
+            const panelData = useMemo(() => {
+              const data = memoizedColors.map((fcm) => fcm.name).join(", ");
+              return <p style={{ fontSize: "15px" }}>{data}</p>;
+            }, [memoizedColors]);
+
+            return (
+              <Accordion buttonLabel="Colors" panelData={panelData} maxHeight={120} />
+            );
+          }
+
+          function DetailAccordion({ colorType }) {
+            const panelData = useMemo(() => {
+              const fontSize = "15px";
+              return colorType.name === SUBTRACTIVE_COLOR_TYPE ? (
+                <>
+                  <p style={{ fontSize }}>
+                    Subtractive colors are created by completely or partially absorbing
+                    <br />
+                    some light wavelengths and reflecting others.
+                  </p>
+                </>
+              ) : (
+                <p style={{ fontSize }}>
+                  Additive colors are created by adding colored light to black.
+                </p>
+              );
+            }, [colorType]);
+
+            return (
+              <Accordion buttonLabel="Detail" panelData={panelData} maxHeight={120} />
+            );
+          }
+
+          function DescriptionAccordions({ colors, colorType }) {
+            return (
+              <>
+                <p>Information</p>
+                <ColorAccordion colors={colors} />
+                <DetailAccordion colorType={colorType} />
+              </>
+            );
+          }
         `),
       },
     ],
@@ -65,7 +161,7 @@ export default function UseContext({ slideIndex, slideOrder }) {
       <section>
         <Header>useContext Demo</Header>
         <CodeDemo structures={useContextCodes}>
-          <UsingUseContextDemo />
+          <UsingUseContextPropDrillDemo />
         </CodeDemo>
       </section>
     </section>
